d8b2471d86c3a69ea32c50ac9c135d8cf82e00fb
brittneypostma <brittney.postma@hotmail.com>
2021-12-20 11:34:04 -0500

    init repo/bug with tabindex -1 on body

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,22 @@
+<script>
+  import '../app.css'
+</script>
+
+<svelte:body tabindex="0" />
+<main>
+  <slot />
+</main>
+
+<style>
+  :global(body) {
+    margin: 0;
+    padding: 0;
+    background: var(--gradient-4);
+  }
+
+  main {
+    display: grid;
+    place-items: center;
+    height: 100vh;
+  }
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,12 @@
+<h1>Welcome to SvelteKit</h1>
+
+<style>
+  h1 {
+    display: inline-block;
+    background-color: var(--gray-8);
+    color: var(--gray-1);
+    padding: var(--size-3) var(--size-6);
+    font-size: var(--font-size-fluid-3);
+    box-shadow: var(--shadow-4);
+  }
+</style>
